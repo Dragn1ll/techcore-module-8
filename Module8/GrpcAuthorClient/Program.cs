d7e248688a1e0b8a7de1abb1c92cbc6be5c069c3
Shakhzod Abdulloev <abdulloev-2005@mail.ru>
2025-11-16 23:41:13 +0300

    feat(task-789): add example grpc

---
+++ Module8/GrpcAuthorClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using Grpc.Net.Client;
+using GrpcAuthorServer;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7039"); // ПРИМЕР АДРЕСА!
+
+var client = new AuthorService.AuthorServiceClient(channel);
+
+Console.WriteLine("Введите ID автора для запроса:");
+var authorId = Console.ReadLine();
+
+var reply = await client.GetAuthorAsync(new GetAuthorRequest { AuthorId = authorId });
+
+Console.WriteLine("Ответ от сервера:");
+Console.WriteLine($"ID: {reply.AuthorId}");
+Console.WriteLine($"Имя: {reply.Name}");
+Console.WriteLine($"Биография: {reply.Biography}");
+
+Console.WriteLine("Нажмите любую клавишу для выхода...");
+Console.ReadKey();
