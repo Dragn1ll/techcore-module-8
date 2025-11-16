79e22511aa020baeed34a48e18ac28a47c612389
Shakhzod Abdulloev <abdulloev-2005@mail.ru>
2025-11-16 23:10:18 +0300

    feat(task-789): add policy handler

--- Module8/Library.Domain/Entry.cs
+++ Module8/Library.Domain/Entry.cs
@@ -1,8 +1,14 @@
-﻿using Library.Domain.Abstractions.Services;
+﻿using System.Net;
+using System.Text;
+using Library.Domain.Abstractions.Services;
 using Library.Domain.Services;
+using Library.SharedKernel.Enums;
+using Library.SharedKernel.Utils;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
 using Polly;
+using Polly.CircuitBreaker;
 
 namespace Library.Domain;
 
@@ -23,6 +29,19 @@ public static class Entry
         )
         .AddTransientHttpErrorPolicy(p =>
             p.CircuitBreakerAsync(5, TimeSpan.FromSeconds(30))
+        )
+        .AddPolicyHandler(Policy<HttpResponseMessage>.Handle<BrokenCircuitException>()
+            .FallbackAsync(
+                fallbackAction: _ =>
+                {
+                    var errorResult = Result<string>.Failure(new Error(ErrorType.ServerError, 
+                        "Не удалось подключиться..."));
+                    var json = JsonConvert.SerializeObject(errorResult);
+                    return Task.FromResult(new HttpResponseMessage(HttpStatusCode.ServiceUnavailable)
+                    {
+                        Content = new StringContent(json, Encoding.UTF8, "application/json")
+                    });
+                })
         );
 
         return serviceCollection;
